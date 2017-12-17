485de439a4673c616b09aa83ac5fbe365dae8b5d
WallaceRocha <wallacedanrley.arc@gmail.com>
2017-12-17 17:18:19 -0300

    Retornando lista de categorias

--- src/main/java/com/wall/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/wall/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,27 @@
 package com.wall.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wall.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		
-		return "Rest funcionando";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria();
+		Categoria c2 = new Categoria(1,"Escritório");
+		c1.setNome("Informática");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		return lista;
 	}
 	
 }
